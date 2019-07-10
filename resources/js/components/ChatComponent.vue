9387d02a91e2386215ef5560cd128ce7b7af3790
Syed Nazir <nazir@golpik.com>
2019-07-10 19:39:48 +0500

    first commit

---
+++ resources/js/components/ChatComponent.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card card-defualt">
+                <div class="card-header">Messages</div>
+                <div class="card-body p-0">
+                    <ul class="list-unstyled" style="height: 300px; overflow-y:scroll">
+                        <li class="p-2" v-for="(item, index) in messages" :key="index">
+                            <strong>{{ item.user.name }}</strong>
+                            {{ item.message }}
+                        </li>
+                    </ul>
+                </div>
+
+                <input
+                    @keyup.enter="sendMessage"
+                    v-model="newMessage"
+                    type="text" 
+                    class="form-control" 
+                    name="message" placeholder="Enter your message...">
+            </div>
+            <span class="text-muted">user is typing...</span>
+        </div>
+        <div class="col-md-4">
+            <div class="card card-defualt">
+                <div class="card-header">Active Users</div>
+                <div class="card-body">
+                    <ul>
+                        <li class="py-2">Hamza</li> 
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['user'],
+        created() {
+            this.fetchMessages()
+
+            Echo.join('chat')
+                .listen('MessageSent', (e) => {
+                    // console.log(e)
+                    this.messages.push(e.message)
+                })
+
+        },
+        data() {
+            return {
+                messages: [],
+                newMessage: ''
+            }
+        },
+        methods: {
+            fetchMessages() {            
+                axios.get('messages').then((response) => {
+                    this.messages = response.data
+                })
+            },
+            sendMessage() {
+
+                this.messages.push({
+                    user: this.user,
+                    message: this.newMessage
+                })
+
+                axios.post('messages', { 'message': this.newMessage})
+
+                this.newMessage = ''
+
+            },
+        },
+    }
+</script>
